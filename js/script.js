8d9f10574f8173e171064a903a443b84bc21ca3e
Rachel Mendes <rachel_nepomuceno@hotmail.com>
2023-10-14 12:04:41 -0300

    sobre

--- js/script.js
+++ js/script.js
@@ -43,23 +43,6 @@ $(document).ready(function(){
   $('[data-toggle="tooltip"]').tooltip();   
 });
 
-// Select all tabs
-$('.nav-tabs a').click(function(){
-  $(this).tab('show');
-})
-
-// Select tab by name
-$('.nav-tabs a[href="#home"]').tab('show')
-
-// Select first tab
-$('.nav-tabs a:first').tab('show')
-
-// Select last tab
-$('.nav-tabs a:last').tab('show')
-
-// Select fourth tab (zero-based)
-$('.nav-tabs li:eq(3) a').tab('show')
-
 
 //Get the button:
 var mybutton = document.getElementById("btnTop");
