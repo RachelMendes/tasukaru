7f0e058ee66a4436d13f6a9d812970cd2e5a7661
Rachel Mendes <rachel_nepomuceno@hotmail.com>
2023-10-27 21:27:48 -0300

    ajuste js

--- js/script.js
+++ js/script.js
@@ -38,25 +38,24 @@ function changeSizeDefault(){
 	}	
 }
 
-//Tooltip
-$(document).ready(function(){
-  $('[data-toggle="tooltip"]').tooltip();   
+$(document).ready(function(){	
+	//Get the button:
+	var mybutton = document.getElementById("btnTop");
+
+	// When the user scrolls down 20px from the top of the document, show the button
+	window.onscroll = function() {scrollFunction()};
+
+	function scrollFunction() {
+	  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		mybutton.style.display = "block";
+	  } else {
+		mybutton.style.display = "none";
+	  }
+	}
+	
+	
 });
 
-
-//Get the button:
-var mybutton = document.getElementById("btnTop");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
