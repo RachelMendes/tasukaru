f0eb4b5587ca30e4cd614dd0a2cd2f6b01e5e917
Rachel Mendes <rachelmendesnepomuceno@gmail.com>
2023-11-19 16:44:27 -0300

    tasukaru final

--- js/script.js
+++ js/script.js
@@ -2,40 +2,42 @@
 function changeSizePlus(){
 	//variável x recebe todos os elementos do tipo parágrafo (tag 'p') 	
 	var x = document.getElementsByTagName('p');
-	var y = document.getElementsByTagName('h3');
+	var y = document.getElementById("modo-uso");
 	var i; //declaração da variável auxiliar i
 	/* 'for' percorre o documento encontrando todos os elementos do tipo parágrafo (tag 'p') 
 	e alterando seu parâmetro 'fontSize' */	
 	for (i = 0; i < x.length; i++) {
 		x[i].style.fontSize = "20px";
 	}	
-	for (i = 0; i < y.length; i++) {
-		y[i].style.fontSize = "1.75rem";
-	}	
+	y.style.fontSize = "20px";	
 }
 	
 //Função que diminui o tamanho da fonte	
 function changeSizeMinus(){
 	//variável x recebe todos os elementos do tipo parágrafo (tag 'p') 	
 	var x = document.getElementsByTagName('p');
+	var y = document.getElementById("modo-uso");	
 	var i; //declaração da variável auxiliar i
 	/* 'for' percorre o documento encontrando todos os elementos do tipo parágrafo (tag 'p') 
 	e alterando seu parâmetro 'fontSize' */	
 	for (i = 0; i < x.length; i++) {
 		x[i].style.fontSize = "12px";
 	}	
+	y.style.fontSize = "12px";	
 }
 
 //Função que restaura o tamanho da fonte	
 function changeSizeDefault(){
 	//variável x recebe todos os elementos do tipo parágrafo (tag 'p') 
 	var x = document.getElementsByTagName('p');
+	var y = document.getElementById("modo-uso");	
 	var i; //declaração da variável auxiliar i
 	/* 'for' percorre o documento encontrando todos os elementos do tipo parágrafo (tag 'p') 
 	e alterando seu parâmetro 'fontSize' */
 	for (i = 0; i < x.length; i++) { 
 		x[i].style.fontSize = "16px";
-	}	
+	}
+	y.style.fontSize = "16px";		
 }
 
 $(document).ready(function(){	
@@ -68,10 +70,10 @@ function topFunction() {
 function activeBttDraw(){	
 	var x = document.getElementById("btn-draw");
 	if (x.getAttribute("app_active") == null){
-		x.style.backgroundColor = "#1a1d20";
+		x.style.backgroundColor = "#da364f";
 		x.setAttribute("app_active", "")
 	} else {
-		x.style.backgroundColor = "#343a40";
+		x.style.backgroundColor = "#242d34";
 		x.removeAttribute("app_active")
 	}
 }
@@ -79,10 +81,10 @@ function activeBttDraw(){
 function activeBttVoice(){	
 	var x = document.getElementById("btn-voice");
 	if (x.getAttribute("app_active") == null){
-		x.style.backgroundColor = "#1a1d20";
+		x.style.backgroundColor = "#da364f";
 		x.setAttribute("app_active", "")
 	} else {
-		x.style.backgroundColor = "#343a40";
+		x.style.backgroundColor = "#242d34";
 		x.removeAttribute("app_active")
 	}
 }
